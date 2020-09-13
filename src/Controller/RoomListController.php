3a02709bab86be5182f0f3dfcecd1bdd36130119
Dmytro Lytvynchuk <dmytrolutv@gmail.com>
2020-09-13 11:32:01 +0300

    Added roomList page

---
+++ src/Controller/RoomListController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\RoomListProviderInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Rendering roomList page.
+ *
+ * @author Dmytro Lytvynchuk <dmytrolutv@gmail.com>
+ */
+class RoomListController extends AbstractController
+{
+    private RoomListProviderInterface $roomListProvider;
+
+    public function __construct(RoomListProviderInterface $roomListProvider)
+    {
+        $this->roomListProvider = $roomListProvider;
+    }
+
+    /**
+     * @Route("/rooms", methods={"GET"}, name="app_rooms")
+     */
+    public function RoomList(): Response
+    {
+        $roomList = $this->roomListProvider->getRoomList();
+
+        return $this->render('roomList/roomList.html.twig');
+    }
+}
---
+++ src/Service/RoomListFakeProvider.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Service;
+
+class RoomListFakeProvider implements RoomListProviderInterface
+{
+    public function getRoomList()
+    {
+        // TODO: Implement getRoomList() method.
+    }
+}
